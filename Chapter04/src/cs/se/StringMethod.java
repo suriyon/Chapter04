55c8b51f2c76041022923c00e1035ee55cc575f5
NAOSUKE <naosuke@somewhere.com>
2017-12-06 09:17:51 +0700

    Second Commit

--- Chapter04/src/cs/se/StringMethod.java
+++ Chapter04/src/cs/se/StringMethod.java
@@ -6,6 +6,15 @@ public class StringMethod {
 		String str = "Tom Hanks";
 		int length = str.length();
 		System.out.println("String Length: " + length);
+		String sub = str.substring(0);
+		System.out.println("Substring: " + sub);
+		String str1 = "Tom Criuse";
+		System.out.println("String Equals: " + str.equals("Tom Hanks"));
+		System.out.println("String Equals: " + str.equals("tom Hanks"));
+		System.out.println("String Equals: " + str.equals(str1));
+		System.out.println("String Compare To: " + str.compareTo(str1));
+		System.out.println("String Compare To: " + str1.compareTo(str));
+		System.out.println("String Compare To: " + str1.compareTo(str1));
 	}
 
 }
