4dc0552d4b7948c8d385881549c95779c47d4cae
NAOSUKE <naosuke@somewhere.com>
2017-12-06 08:50:51 +0700

    First Commit

--- Chapter04/src/cs/se/StringMethod.java
+++ Chapter04/src/cs/se/StringMethod.java
@@ -3,7 +3,9 @@ package cs.se;
 public class StringMethod {
 
 	public static void main(String[] args) {
-		
+		String str = "Tom Hanks";
+		int length = str.length();
+		System.out.println("String Length: " + length);
 	}
 
 }
